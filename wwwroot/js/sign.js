0a9b668038b138e2b6ee95650a06d38d8c1a83d8
Anthony Mogotlane <almogotlane@gmail.com>
2022-12-01 08:03:42 +0200

    Updated the screen and Modified the functionality of the game

--- wwwroot/js/sign.js
+++ wwwroot/js/sign.js
@@ -2,8 +2,46 @@
 const curResult = document.querySelector(".cur-result"); // Result output
 const scoreContainer = document.querySelector(".score-container"); // score container
 const score = document.querySelector(".score"); // score
+const startArrow = document.querySelector(".start-arrow");
+const startBtn = document.querySelector(".start-btn");
+const challengeContainer = document.querySelector(".challenge-container");
+const challenge = document.querySelector(".challenge");
+const nextBtn = document.querySelector(".next-btn");
+const progressContainer = document.querySelector(".progress-container");
+const timer = document.querySelector(".timer");
+const body = document.querySelector("body");
+const tryBtn = document.querySelector(".try-btn");
+const fireworks = document.querySelector(".pyro");
+const playBtn = document.querySelector(".play-btn");
+
+// variables
 let playerScore = 0;
 let index = 0;
+let progress = 0;
+
+const timeLimit = 15;
+let countDown = timeLimit;
+
+let timerInterval;;
+
+function theTimer() {
+  if (countDown > 0) {
+    countDown--;
+    console.log(countDown)
+    timer.innerHTML = `${countDown}s`
+  } else {
+    tryBtn.classList.remove("d-none");
+    curResult.innerHTML = `<h2>Time up</h2>`;
+
+    body.classList.remove("bg-white");
+    body.classList.remove("bg-success");
+    body.classList.add("bg-danger");
+  }
+}
+
+function stopper() {
+  clearInterval(timerInterval)
+}
 
 // TM code
 // Link to model
@@ -41,30 +79,50 @@ async function predict() {
   // predict can take in an image, video or canvas html element
   const prediction = await model.predict(webcam.canvas);
 
+  progress = Math.round(prediction[index].probability * 100);
+  progressContainer.innerHTML = `  
+    <div class="progress">
+      <div class="progress-bar bg-success progress-bar-striped" role="progressbar" style="width: ${progress}%;" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">${progress}%</div>
+    </div>`
+
   if (
     prediction[index].className === challenges[index - 1].answer &&
     prediction[index].probability >= 0.95
   ) {
-    console.log("yes");
     curResult.innerHTML = `<h2>👌Great</h2>`;
     nextBtn.classList.remove("d-none");
+
+    
+    stopper();
+    
+    body.classList.remove("bg-white");
+    body.classList.remove("bg-danger");
+    body.classList.add("bg-success");
+
+    if(index === 5) {
+      challengeContainer.classList.add("d-none");
+      nextBtn.classList.add("d-none");
+      timer.classList.add("d-none");
+
+      body.classList.remove("bg-success");
+      fireworks.classList.remove("d-none");
+      playBtn.classList.remove("d-none");
+
+    }
   }
 }
 
-const startBtn = document.querySelector(".start-btn");
-const challengeContainer = document.querySelector(".challenge-container");
-const challenge = document.querySelector(".challenge");
-const nextBtn = document.querySelector(".next-btn");
-
 const startChallenge = () => {
   startBtn.classList.add("d-none");
+  startArrow.classList.add("d-none");
   challengeContainer.classList.remove("d-none");
+  timer.classList.remove("d-none");
+
   nextSign();
   init();
-  document.querySelector(".controllers").classList.remove("controllers");
-  scoreContainer.classList.remove("d-none");
 };
 startBtn.addEventListener("click", startChallenge);
+
 
 // Moving to the next sign
 const nextSign = () => {
@@ -74,13 +132,38 @@ const nextSign = () => {
   <h4 class="text-center">Alphabet to sign</h4>
   <img src=${challenges[index].image} class="challenge-img" alt="A-Sign" />
   </div>`;
-  
+
+  progressContainer.innerHTML = `  
+    <div class="progress">
+      <div class="progress-bar bg-success progress-bar-striped" role="progressbar" style="width: ${progress * 100}%;" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">${progress}%</div>
+    </div>`
+
   index++;
   nextBtn.classList.add("d-none");
   curResult.innerHTML = `<h2>👉</h2>`;
   score.innerHTML = `${parseInt(score.textContent) + 10}pts`;
+  body.classList.remove("bg-success");
+  body.classList.add("bg-white");
+
+  countDown = timeLimit;
+  timer.innerHTML = `${countDown}s`;
+  timerInterval = setInterval(theTimer, 1000);
 };
 nextBtn.addEventListener("click", nextSign);
+
+// Try again function
+const trainAgain = () => {
+  countDown = 15;
+
+  curResult.innerHTML = `<h2>👉</h2>`;
+
+  tryBtn.classList.add("d-none");
+  body.classList.add("bg-white");
+  body.classList.remove("bg-danger");
+  body.classList.remove("bg-success");
+}
+tryBtn.addEventListener("click", trainAgain);
+
 
 // Challenge questions
 const challenges = [
@@ -105,3 +188,6 @@ const challenges = [
     answer: "E",
   },
 ];
+
+
+
