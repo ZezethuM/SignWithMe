069f33fa212f4413656288b62cfb9469ea49972e
Teboho M <teboho.motsamai@Opsi.co.za>
2022-12-01 11:43:08 +0200

    [DEV] Did some changes

--- Program.cs
+++ Program.cs
@@ -5,11 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-//builder.Services.AddControllers();
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-<<<<<<< HEAD
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 string connString = builder.Configuration.GetConnectionString("TBConnection");
 builder.Services.AddSingleton<IQuestions>(new QuestionsRepo(connString) );
 builder.Services.AddSingleton<ISigns>(new SignsRepo(connString) );
@@ -21,30 +21,15 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
 }
-=======
-// builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
->>>>>>> 7ff5c28d350f2824c0badb0dba673c434aae19a8
-
-// To display static files
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
+ app.UseHttpsRedirection();
 
-// app.UseHttpsRedirection();
+ app.UseAuthorization();
 
-// app.UseAuthorization();
-
-// app.MapControllers();
+ app.MapControllers();
 
 app.Run();
